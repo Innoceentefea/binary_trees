e47812b57c58d89f9457d411a50f3c55d5e9bade
muirujackson <jakezweru@gmail.com>
2023-08-29 03:17:48 -0700

    change the parent node of prev node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,8 +21,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	newNode->right = NULL;
 	if (parent->left)
 	{
+		parent->left->parent = newNode;
 		newNode->left = parent->left;
-		parent->left = newNode;
 	}
 	parent->left = newNode;
 
