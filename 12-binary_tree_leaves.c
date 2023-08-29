db5d6ec77fbb0d7549c530df084948c910e4bded
muirujackson <jakezweru@gmail.com>
2023-08-29 15:29:31 -0700

    redone number 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,15 +7,19 @@
  * @tree: Pointer to root node of tree to count number of leaves.
  * Return: 0 if tree is NULL else number of leaves in tree.
  */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t count_l_leaf = 0, count_r_leaf = 0;
+
 	if (!tree)
 		return (0);
+
+	count_l_leaf = binary_tree_leaves(tree->left);
+	count_r_leaf = binary_tree_leaves(tree->right);
+
 	if (!tree->left && !tree->right)
-		return (1);
-
-	size_t count_left_leaves = binary_tree_leaves(tree->left);
-	size_t count_right_leaves = binary_tree_leaves(tree->right);
-
-	return (count_left_leaves + count_right_leaves);
+		return (count_l_leaf + count_r_leaf + 1);
+	else
+		return (count_l_leaf + count_r_leaf);
 }
