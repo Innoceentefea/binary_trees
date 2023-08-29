c5dec08e84e96e254c490915e89cdd19076afff1
muirujackson <jakezweru@gmail.com>
2023-08-29 06:18:38 -0700

    delete task

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,7 +8,23 @@
  * Return: A pointer to the delete node
  */
 
- void binary_tree_delete(binary_tree_t *tree)
- {
-    if tree is NULL()
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_t *tmp1, *tmp = tree;
+
+	while (tree)
+	{
+		while (tmp->left)
+		{
+			tmp = tmp->left;
+		}
+		tmp1 = tmp;
+		if (tmp1->parent)
+			tmp = tmp1->parent;
+		free(tmp1);
+		if (tmp->right)
+			tmp = tmp->right;
+	}
  }
