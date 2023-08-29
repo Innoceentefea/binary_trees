c97aa6f75c750e6b457fcd0f77515e69294edeac
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-08-29 13:50:21 +0100

    insert_right

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function to delete
+ * @parent: the parent node
+ * @value: the number of the new node
+ *
+ * Return: A pointer to the delete node
+ */
+
+ void binary_tree_delete(binary_tree_t *tree)
+ {
+    if tree is NULL()
+ }
