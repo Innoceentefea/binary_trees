4284167f1ce57d6abadeb4af417abdf27e715c76
muirujackson <jakezweru@gmail.com>
2023-08-29 02:33:59 -0700

    check value exist

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-	if (!newNode)
+	if (!newNode || !value)
 		return (NULL);
 	newNode->parent = parent;
 	newNode->n = value;
