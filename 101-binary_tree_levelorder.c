0560291860a1f05f64b4deb63343552aa47f56de
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-09-02 01:38:09 +0100

    task 21

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -46,7 +46,8 @@ void print_level(binary_tree_t *tree, int level, void (*func)(int))
 }
 
 /**
- * binary_tree_levelorder - Traverses a binary tree using level-order traversal.
+ * binary_tree_levelorder - 
+   Traverses a binary tree using level-order traversal.
  * @tree: Pointer to the root node of the tree to traverse.
  * @func: Pointer to a function to call for each node.
  */
